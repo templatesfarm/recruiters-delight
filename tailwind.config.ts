a28cb78b7f4d9a3ee54a132cb1b87e960f506568
DeepakPahawa <deepak.pahawa1911@gmail.com>
2025-01-23 19:09:40 +0530

    Fixed Tailwind CSS Class recognizing issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/portfolioui/dist/*.js",
+    "./node_modules/portfolioui/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
